e4fbe0ffa55dc97ab3d73389c585d60316766092
Nathan Sepulveda <nathans.composer@gmail.com>
2020-07-12 09:04:37 -0500

    Initial Commit

---
+++ Basic-SynthTests/Basic_SynthTests.swift
@@ -0,0 +1,34 @@
+//
+//  Basic_SynthTests.swift
+//  Basic-SynthTests
+//
+//  Created by Nathan Sepulveda on 7/12/20.
+//  Copyright © 2020 Nathan Sepulveda. All rights reserved.
+//
+
+import XCTest
+@testable import Basic_Synth
+
+class Basic_SynthTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
